52db0802aa305fafa512d47fe503ad2c219d4855
Harry101969 <harshagarwal19696@gmail.com>
2024-09-25 04:12:48 +0530

    Updated

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-
 import { PrismaClient } from "@prisma/client";
 import express from "express";
 
@@ -8,22 +7,26 @@ app.use(express.json());
 const client = new PrismaClient();
 
 app.get("/", (req, res) => {
-    res.json({
-        message: "Healthy server"
-    })
-})
+  res.json({
+    message: "Healthy server",
+  });
+});
 
 app.post("/", async (req, res) => {
+  try {
     await client.user.create({
-        data: {
-            email: req.body.email,
-            name: req.body.name
-        }
-    })
+      data: {
+        email: req.body.email,
+        name: req.body.name,
+      },
+    });
 
     res.json({
-        message: "Done signing up!"
-    })
-})
+      message: "Done signing up!",
+    });
+  } catch (error) {
+    res.json({ error });
+  }
+});
 
 app.listen(3000);
